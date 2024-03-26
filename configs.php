285b34b7259ef32be5a917c265dc82eabc96c6c5
Yuri Timafejn <yuri.timafejn@ecgroup.com>
2024-03-26 11:28:09 -0300

    feat: Added static class to merge pdf files

---
+++ configs.php
@@ -0,0 +1,3 @@
+<?php
+
+const PATH_FINAL = __DIR__ . '/files/';
--- src/PHP_Handle.php
+++ src/PHP_Handle.php
@@ -1,6 +1,43 @@
 <?php
 
+namespace Yurit\PdfHandler;
+
+use ImagickException;
+
 class PHP_Handle
 {
+    /**
+     * @param string $finalFileName
+     * @param array $pdfPaths
+     * @return int|false
+     * @throws ImagickException
+     */
+    public static function MergePDF(string $finalFileName, $pdfPaths): int|false
+    {
+        $mergePDF = new \Imagick();
 
+        foreach ($pdfPaths as $pdf) {
+            $tmp = new \Imagick();
+            $tmp->readImage($pdf);
+
+            $mergePDF->addImage($tmp);
+            $tmp->clear();
+            $tmp->destroy();
+        }
+
+        $mergePDF->setFormat('PDF');
+        $mergePDF->setImageCompressionQuality(100);
+        $mergePDF->setImageCompression(70);
+
+        $tmpFile = tempnam(sys_get_temp_dir(), 'merge_');
+        $mergePDF->writeImages($tmpFile, true);
+
+        $result = file_put_contents(PATH_FINAL . $finalFileName . '.pdf', file_get_contents($tmpFile));
+
+        unlink($tmpFile);
+        $mergePDF->clear();
+        $mergePDF->destroy();
+
+        return $result;
+    }
 }
